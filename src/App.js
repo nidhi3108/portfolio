2cf7a6f2278c0a7af3b645dfb270c6d9ef6ee0ac
Divyansh <divyansh.verma@itinker.io>
2024-03-10 13:25:25 +0530

    Fixed the scroll

--- src/App.js
+++ src/App.js
@@ -7,43 +7,52 @@ import Skill from "./component/Skills/Skill";
 import Contact from "./component/Contact/Contact";
 import Footer from "./component/Footer/Footer";
 import Education from "./component/Education/Education.js";
-import { themeContext } from "./Context";
-import { useContext } from "react";
-import { BrowserRouter, Routes,Route } from "react-router-dom";
+import {themeContext} from "./Context";
+import {useContext} from "react";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+
 function App() {
   const theme = useContext(themeContext);
   const darkMode = theme.state.darkMode;
   return (
-    <div
-      className="App"
-      style={{
-        background: darkMode ? "black" : "",
-        color: darkMode ? "white" : "",
-      }}
-    >
-      <Navbar/>
-      <Intro/>
-      <Education/>
-      <Skill/>
-      <Experience/>
-      <Project/>
-      <Contact/>
-      <Footer/>
-      {/* <BrowserRouter> */}
-    {/* <Header/> */}
-    {/* <Routes>
-      <Route element={<Navbar/>} path="/"/>
-      <Route element={<Intro/>} path="/intro"/>
-      <Route element={<Education/>} path="education"/>
-      <Route element={<Skill/>} path="/skill"/>
-      <Route element={<Experience/>} path="/experience"/>
-      <Route element={<Project/>} path="/project"/>
-      <Route element={<Contact/>} path="/contact"/>
-      <Route element={<Footer/>} path="/footer"/>
-    </Routes>
-    </BrowserRouter> */}
-    </div>
-  
+
+    <>
+      <div
+        className="App"
+        style={{
+          background: darkMode ? "black" : "",
+          color: darkMode ? "white" : "",
+        }}
+      >
+
+
+        <BrowserRouter>
+        <Navbar/>
+
+          <div id={"home"}>
+            <Intro/>
+          </div>
+          <div id={"education"}>
+            <Education id={"education"}/>
+          </div>
+          <div id={"skills"}>
+            <Skill id={"skills"}/>
+          </div>
+          <div id={"experience"}>
+            <Experience id={"experience"}/>
+          </div>
+          <div id={"project"}>
+            <Project id={"project"}/>
+          </div>
+          <div id={"contact"}>
+            <Contact id={"contact"}/>
+          </div>
+          <Footer/>
+
+        </BrowserRouter>
+      </div>
+    </>
+
   );
 }
 
--- src/component/Navbar/Navbar.js
+++ src/component/Navbar/Navbar.js
@@ -1,31 +1,29 @@
 import React from 'react'
 import './Navbar.css';
-import {NavLink} from "react-router-dom";
 import Toggle from '../Toggle/Toggle';
-// import { NavLink } from 'react-router-dom';
+import {Link} from 'react-scroll'
 
-const Navbar=()=>{
-    return(
-        <div className='n-wrapper'>
-            <div className="n-left">
-                <div className="n-name">xyz</div>
-                <Toggle/>
-            </div>
-            <div className="n-right">
-               <div className="n-list">
-                <ul style={{listStyleType:"none"}}>
-                   <li>Home</li>
-                    <li>Education</li>
-                    <li>Skills</li>
-                    <li>Experience</li>
-                    <li>Project</li>
-                    {/* <li><NavLink to="/contact"></NavLink></li> */}
-                </ul>
-               </div>
-               <button className="button n-button">Contact-us</button>
-            </div>
+const Navbar = () => {
+  return (
+    <div className='n-wrapper'>
+      <div className="n-left">
+        <div className="n-name">xyz</div>
+        <Toggle/>
+      </div>
+      <div className="n-right">
+        <div className="n-list">
+          <ul style={{listStyleType: "none"}}>
+            <li><Link to="home" spy={true} smooth={true}>Home</Link></li>
+            <li><Link to="education" spy={true} smooth={true}>Education</Link></li>
+            <li><Link to="skills" spy={true} smooth={true}>Skills</Link></li>
+            <li><Link to="experience" spy={true} smooth={true}>Experience</Link></li>
+            <li><Link to="project" spy={true} smooth={true}>Project</Link></li>
+          </ul>
         </div>
-    )
+        <button className="button n-button"><Link to="contact" spy={true} smooth={true}>Contact-us</Link></button>
+      </div>
+    </div>
+  )
 }
 
 export default Navbar
